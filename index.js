60c9eeb6e4e6b6794876dcd3976fc6d21d9dd9f2
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2019-10-25 12:27:12 -0600

    modified

---
+++ index.js
@@ -0,0 +1,8 @@
+//'use strict';
+// I’m a start quiz button. 
+// I listen for the eventHandler ‘click’ target function. 
+// When I see the function has been activated I call the page to switch to quiz question 1.
+
+//radio button function that does a for loop through the array of questions and increments as answers are handed back by the user
+
+//
