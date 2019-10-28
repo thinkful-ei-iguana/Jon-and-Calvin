a8e0a353ca14b4cd4c6be6991bc085a4e9eddc19
Jon <jonathanjackson424@gmail.com>
2019-10-28 10:51:22 -0400

    added question content

--- final.js
+++ final.js
@@ -15,7 +15,7 @@ nextButton.addEventListener('click', () => {
 
 function startGame() {
   startButton.classList.add('hide');
-  shuffledQuestions = questions.sort(() => Math.random() - .5);
+  shuffledQuestions = questions;
   currentQuestionIndex = 0;
   questionContainerElement.classList.remove('hide');
   setNextQuestion();
@@ -81,61 +81,73 @@ const questions = [
   {
     question: 'https://i.pinimg.com/originals/2f/53/7a/2f537a79b3222328ea148c68b30425cd.png',
     answers: [
-      { text: '4', correct: true },
-      { text: '22', correct: false }
+      { text: ' i4/3', correct: false },
+      { text: 'i6', correct: true},
+      { text: 'VII', correct: false},
+      { text: 'III', correct: false }
     ]
   },
   {
     question: 'https://i.pinimg.com/originals/cd/06/02/cd0602ae9c79e2a8c696d200f32716d6.png',
     answers: [
-      { text: 'Web Dev Simplified', correct: true },
-      { text: 'Traversy Media', correct: true },
-      { text: 'Dev Ed', correct: true },
-      { text: 'Fun Fun Function', correct: true }
+      { text: 'Perfect Fourth', correct: false },
+      { text: 'Diminished Fourth', correct: false },
+      { text: 'Augmented Fourth', correct: true },
+      { text: 'Major Third', correct: false }
     ]
   },
   {
     question: 'https://i.pinimg.com/originals/a8/f0/84/a8f0846debfe7e74acacc97c2eb49422.png',
     answers: [
-      { text: 'Kinda', correct: false },
-      { text: 'YES!!!', correct: true },
-      { text: 'Um no', correct: false },
-      { text: 'IDK', correct: false }
+      { text: 'Doubled Leading Tone', correct: false },
+      { text: 'Voice Crossing', correct: false },
+      { text: 'Parallel Octaves', correct: false },
+      { text: 'Parallel Fifths', correct: true }
     ]
   },
   {
     question: 'https://i.pinimg.com/originals/af/fd/32/affd32af6ef956b99b06213f12b3baa7.png',
     answers: [
-      { text: '6', correct: false },
-      { text: '8', correct: true }
+      { text: 'Major Seventh', correct: false },
+      { text: 'Minor Seventh', correct: false },
+      { text: 'Diminished Seventh', correct: true },
+      { text: 'Half-Diminished Seventh', correct: false }
     ]
   },
   {
     question: 'https://i.pinimg.com/originals/1c/2d/3c/1c2d3c6ff688352b5b6ceef39960d634.png',
     answers: [
-      { text: '6', correct: false },
-      { text: '8', correct: true }
+      { text: 'Doubled Third', correct: true },
+      { text: 'Soprano voice is out of range', correct: false },
+      { text: 'Tripled Root', correct: false },
+      { text: 'Incomplete Chord', correct: false }
     ]
   },
   {
     question: 'https://i.pinimg.com/originals/3f/cf/03/3fcf03cee944a18eea064e71014e482f.png',
     answers: [
-      { text: '6', correct: false },
-      { text: '8', correct: true }
+      { text: 'Marcato', correct: false },
+      { text: 'Legato', correct: false },
+      { text: 'Staccato', correct: true },
+      { text: 'Tenuto', correct: false }
     ]
   },
   {
     question: 'https://i.pinimg.com/originals/7e/87/65/7e8765c1a7a1dd15586ed930bb0fcda5.png',
     answers: [
-      { text: '6', correct: false },
-      { text: '8', correct: true }
+      { text: 'V6/5', correct: true },
+      { text: 'V4/3', correct: false },
+      { text: 'V6/4', correct: false },
+      { text: 'vii°', correct: false }
     ]
   },
   {
     question: 'https://i.pinimg.com/originals/04/12/6f/04126f6071993ecb9c8b5640cde024fd.png',
     answers: [
-      { text: '6', correct: false },
-      { text: '8', correct: true }
+      { text: 'Diminished Seventh', correct: false },
+      { text: 'Augmented Seventh', correct: false },
+      { text: 'Minor Seventh', correct: false },
+      { text: 'Major Seventh', correct: true }
     ]
   },
 ];
